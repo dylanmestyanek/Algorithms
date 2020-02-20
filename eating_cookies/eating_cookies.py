6c42946ac9d713f4314dc944ca65e4900d3869f0
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-20 11:42:51 -0600

    Fixed poor indentations in eating cookies challenge

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -9,9 +9,9 @@ def eating_cookies(n, cache={}):
   if n == 0:
       return 1
   elif n <= 2:
-    return n
+      return n
   elif n in cache:
-        return cache[n]
+      return cache[n]
   
   value = eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
   cache[n] = value
