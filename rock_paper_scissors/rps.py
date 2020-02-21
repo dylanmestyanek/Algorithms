78b069a49328d9801fac550d225eb38e0a45d5ae
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-20 19:11:37 -0600

    RPS challenge complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python
-
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    moves = ["rock", "paper", "scissors"]
+    permutations = []
+    result = []
+
+    if n == 0:
+        return [[]]
+
+    def form_permutations(arr, rounds):
+        if rounds > 0:
+          for move in moves:
+              form_permutations(arr + [move], rounds - 1)
+        else:
+            result.append(arr)
+            return arr
 
 
+    form_permutations(permutations, n)
+    return result
+  
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_plays = int(sys.argv[1])
